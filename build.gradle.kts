3df35b8f7842162d20f96ad6247c85df0d0da19e
Max Oliynick <maksimolejn720@gmail.com>
2023-04-19 17:18:39 +0300

    Remove nebula to fix Gradle configuration cache issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,6 @@ plugins {
     id("org.jetbrains.compose") version "1.3.0"
     id("org.jlleitschuh.gradle.ktlint") version "10.0.0" apply false
     id("org.jetbrains.changelog") version "1.3.0"
-    id("nebula.maven-publish") version "18.0.0"
-    id("nebula.source-jar") version "18.0.0"
-    id("nebula.contacts") version "5.1.0"
-    id("nebula.info") version "11.0.1"
     id("com.bybutter.sisyphus.project") version "1.3.33"
     `java-library`
 }
@@ -60,17 +56,4 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
         "-P",
         "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=true"
     )
-}
-
-changelog {
-    version.set(project.version.toString())
-    groups.set(emptyList())
-}
-
-contacts {
-    addPerson("higan@live.cn", delegateClosureOf<nebula.plugin.contacts.Contact> {
-        moniker = "higan"
-        github = "devkanro"
-        roles.add("owner")
-    })
 }
